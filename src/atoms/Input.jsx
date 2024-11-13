1317c2e579e2caab7da2b7c08be5f76fe7592ac4
davidtavartk <d.tavartkiladze5@gmail.com>
2024-11-13 19:58:17 +0400

    name damaviwkda wameshala shemtxvevit ro gadameca

--- src/atoms/Input.jsx
+++ src/atoms/Input.jsx
@@ -1,16 +1,15 @@
-const Input = ({ title, placeholder, className, type, maxLength, onChange, value }) => {
+const Input = ({ title, placeholder, className, type, maxLength, onChange, value, name }) => {
   return (
     <div className="flex flex-col gap-[9px]">
-      <h3 className="text-xs font-medium tracking-[2px] text-[#21092F] uppercase">
-        {title}
-      </h3>
+      <h3 className="text-xs font-medium tracking-[2px] text-[#21092F] uppercase">{title}</h3>
       <input
-        className={`text-lg font-medium border rounded-lg border-[#DFDEE0] px-[16px] py-2 text-[#21092F] placeholder-[#21092F] placeholder-opacity-25 ${className} }`}
+        className={`text-lg font-medium border rounded-lg border-[#DFDEE0] px-[16px] py-2 text-[#21092F] placeholder-[#21092F] placeholder-opacity-25 ${className}`}
         type={type}
         placeholder={placeholder}
         maxLength={maxLength}
         value={value}
         onChange={onChange}
+        name={name}
       />
     </div>
   );
